ebf32cb6488cb6fcaf57b2161fb72b96ac20c995
salsabilatuadaa <100354900+salsabilatuadaa@users.noreply.github.com>
2023-03-08 19:40:22 +0700

    Add files via upload

---
+++ Praktikum 3/org/main/MPoligon.java
@@ -0,0 +1,22 @@
+/** 
+ * File : MPoligon.java 03/08/2023
+ * Penulis : Salsabila Tuada
+ * Deskripsi : driver class untuk poligon dan persegi panjang
+**/
+
+package org.main;
+
+import org.bangundatar.*;
+
+public class MPoligon{
+	public static void main(String[] args){
+		PersegiPanjang persegi = new PersegiPanjang(10,10,4);
+		persegi.printInfo();
+		System.out.println("Luas Persegi Panjang : " +persegi.hitungLuas());
+
+		Segitiga segitiga = new Segitiga(10,10,3);
+		segitiga.printInfo();
+		System.out.println("Luas Segitiga : " +segitiga.hitungLuas());
+	}
+
+}
---
+++ Praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,15 @@
+/** 
+ * File : Poligon.java 03/08/2023
+ * Penulis : Salsabila Tuada
+ * Deskripsi : representasi dasar dari objek poligon (segi banyak)
+**/
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi; //protected mirip private, kalo private hanya bsa di akses di 
+
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
